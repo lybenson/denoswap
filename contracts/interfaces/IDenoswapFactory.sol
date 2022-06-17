74ed3e256972ccf5c51b7ec6b0161b6729e2b09e
lybenson <757766483@qq.com>
2022-06-18 02:35:55 +0800

    remove all

--- contracts/interfaces/IDenoswapFactory.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: GPL-3.0
-
-pragma solidity >=0.7.0 <0.9.0;
-
-interface IDenoswapFactory {
-  function createPair(address tokenA, address tokenB) external returns(address pair);
-}
